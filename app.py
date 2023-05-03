5e102b4763bf24968e0c0384e0bb1ff77f5f5d0e
bbland1 <104288486+bbland1@users.noreply.github.com>
2023-05-03 14:33:13 -0400

    creating a random rotue to show random cafe every call

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import random
 from flask import Flask, jsonify, render_template, request
 from flask_migrate import Migrate
+from sqlalchemy.sql.expression import func
 
 from models import Cafe, db
 
@@ -13,19 +15,30 @@ db.init_app(app)
 
 migrate = Migrate(app, db)
 
+# with app.app_context():
+#     db.create_all()
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
 
 # a route to pull a random cafe from sample data
 @app.route("/random")
-def random():
-    pass
+def random_cafe():
+    # look into the database
+    # order the database randomly
+    # take the first row
+    random_cafe = db.session.execute(db.select(Cafe).order_by(func.random())).first()[0]
+    # use the made dictionary method in the model to change info to dict
+    cafe_dict = random_cafe.as_dict()
+    # return json info
+    return jsonify(cafe_dict)
+
 ## HTTP GET - Read Record
 
 ## HTTP POST - Create Record
+
 
 ## HTTP PUT/PATCH - Update Record
 
@@ -33,4 +46,4 @@ def random():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- models.py
+++ models.py
@@ -15,3 +15,18 @@ class Cafe(db.Model):
     has_sockets = db.Column(db.Boolean, nullable=False)
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'map_url': self.map_url,
+            'img_url': self.img_url,
+            'location': self.location,
+            'seats': self.seats,
+            'has_toilet': self.has_toilet,
+            'has_wifi': self.has_wifi,
+            'has_sockets': self.has_sockets,
+            'can_take_calls': self.can_take_calls,
+            'coffee_price': self.coffee_price,
+        }
